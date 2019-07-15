18ebce10c8b676dcea38ef915ee4f12fcc666291
Nic Gibson <nic@shpangy.com>
2019-07-15 14:23:38 -0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nic Gibson on 7/15/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
